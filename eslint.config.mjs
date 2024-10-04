14be48fceea80d9919341842ce0d89c3ed1a70c2
Guillermo Polit <guiyep@gmail.com>
2024-10-04 15:54:55 +1000

    setup 4

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -1,11 +1,13 @@
 import eslint from '@eslint/js';
 import tseslint from 'typescript-eslint';
 import eslintConfigPrettier from 'eslint-config-prettier';
+import importPlugin from 'eslint-plugin-import';
 
 export default tseslint.config(
   eslint.configs.recommended,
   ...tseslint.configs.strictTypeChecked,
   ...tseslint.configs.stylisticTypeChecked,
+  importPlugin.flatConfigs.recommended,
   eslintConfigPrettier,
   {
     languageOptions: {
@@ -17,10 +19,25 @@ export default tseslint.config(
         projectService: true,
         tsconfigRootDir: import.meta.dirname,
       },
+      ecmaVersion: 'latest',
+      sourceType: 'module',
     },
   },
   {
     files: ['**/*.{js,jsx,cjs,mjs}', '*.{js,jsx,cjs,mjs}'],
     extends: [tseslint.configs.disableTypeChecked],
   },
+  rulesConfigOverrides,
 );
+
+const rulesConfigOverrides = {
+  rules: {
+    'eol-last': ['error', 'always'],
+    'import/prefer-default-export': 'off',
+    'import/no-unresolved': 'off',
+    'import/no-extraneous-dependencies': ['error', { devDependencies: true }],
+    'no-plusplus': 'off',
+    'no-use-before-define': 'off',
+    'prefer-destructuring': 'off',
+  },
+};
